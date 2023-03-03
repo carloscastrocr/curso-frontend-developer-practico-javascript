4437d8e7b5687a5dc012f115e87282214076f88b
Carlos <carloscastroramos.cr@gmail.com>
2023-03-03 11:16:41 -0400

    Lista de productos

---
+++ main.js
@@ -0,0 +1,100 @@
+const menuEmail = document.querySelector('.navbar-email')
+const menuIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+
+const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
+
+const cardsContainer = document.querySelector('.cards-container')
+
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+menuIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleProductDetail)
+
+function toggleDesktopMenu() {
+    const iSasideClosed = aside.classList.contains('inactive')
+
+    if (!iSasideClosed) {
+        aside.classList.add('inactive')
+    } 
+    desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu() {
+    const iSasideClosed = aside.classList.contains('inactive')
+
+    if (!iSasideClosed) {
+        aside.classList.add('inactive')
+    } 
+    mobileMenu.classList.toggle('inactive')
+}
+function toggleProductDetail() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }else if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }    
+        aside.classList.toggle('inactive')
+
+}
+
+const productList= [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'TV',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Car',
+    price: 35000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+     
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+     
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+     
+        const productInfoDiv = document.createElement('div');
+     
+        const productPrice = document.createElement('p');
+        productPrice.innerText= '$' + product.price
+        
+        const productName = document.createElement('p');
+        productName.innerText= product.name
+     
+        productInfoDiv.append(productPrice,productName);
+     
+        const productInfoFigure = document.createElement('figure');
+     
+        const productFigureImg = document.createElement('img');
+        productFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+     
+        productInfoFigure.appendChild(productFigureImg);
+     
+        productInfo.append(productInfoDiv,productInfoFigure)
+     
+        productCard.append(productImg,productInfo)
+        
+        cardsContainer.appendChild(productCard)
+     }
+}
+
+renderProducts(productList);
